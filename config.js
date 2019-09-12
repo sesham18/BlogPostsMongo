40c3e1c09a091031ecfb1a16835be13fb6ffcdef
Sesha Machiraju <sesha.machiraju@avanade.com>
2019-09-12 10:38:00 -0700

    added

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 exports.DATABASE_URL =
   process.env.DATABASE_URL || "mongodb://localhost/blogposts";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/blogposts";
+  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-blogposts";
 exports.PORT = process.env.PORT || 8080;
